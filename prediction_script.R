2f4ee8c16c63b5b11913633fad0ec9c6ba5557e5
akritikts <akritikts@gmail.com>
2018-03-15 14:53:47 +0530

    women and children saved

--- prediction_script.R
+++ prediction_script.R
@@ -18,4 +18,15 @@ manip <- function(){
   train$Child[train$Age < 18] <- 1
   aggregate(Survived ~ Child + Sex, data = train, FUN = sum)
   aggregate(Survived ~ Child + Sex, data = train, FUN = length)
+  aggregate(Survived ~ Child + Sex, data = train, FUN = function(x){sum(x)/length(x)})
+  train$Fare2 <- '30+'
+  train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+  train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+  train$Fare2[train$Fare < 10] <- '<10'
+  aggregate(Survived ~ Fare2 + Pclass + Sex, data = train, FUN = function(x){sum(x)/length(x)})
+  test$Survived <- 0
+  test$Survived[test$Sex == 'female'] <- 1
+  test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+  sub_pred <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+  write.csv(sub_pred, file = "theyallperish.csv", row.names = FALSE)
 }
