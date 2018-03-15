ad8f3f1498f7eb6a9ac6e34337c0349a37a30e00
akritikts <akritikts@gmail.com>
2018-03-15 12:30:18 +0530

    aggregated data for the saved ones

--- prediction_script.R
+++ prediction_script.R
@@ -5,10 +5,17 @@ manip <- function(){
   prop.table(table(train$Survived))
   test$Survived <- rep(0, 418)
   sub_pred <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
-  > write.csv(sub_pred, file = "theyallperish.csv", row.names = FALSE)
+  write.csv(sub_pred, file = "theyallperish.csv", row.names = FALSE)
   summary(train$Sex)
   prop.table(table(train$Sex, train$Survived))
   prop.table(table(train$Sex, train$Survived), 1)
   test$Survived <- 0
   test$Survived[test$Sex == 'female'] <- 1
+  sub_pred <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+  write.csv(sub_pred, file = "theyallperish.csv", row.names = FALSE)
+  summary(train$Age)
+  train$Child <- 0
+  train$Child[train$Age < 18] <- 1
+  aggregate(Survived ~ Child + Sex, data = train, FUN = sum)
+  aggregate(Survived ~ Child + Sex, data = train, FUN = length)
 }
