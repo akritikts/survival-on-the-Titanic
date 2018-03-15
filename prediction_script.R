c250b96f8a6f93cc1c94cb54c44c26bbc1d4898a
akritikts <akritikts@gmail.com>
2018-03-15 12:04:27 +0530

    most of the female passengers were saved

--- prediction_script.R
+++ prediction_script.R
@@ -6,4 +6,9 @@ manip <- function(){
   test$Survived <- rep(0, 418)
   sub_pred <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
   > write.csv(sub_pred, file = "theyallperish.csv", row.names = FALSE)
+  summary(train$Sex)
+  prop.table(table(train$Sex, train$Survived))
+  prop.table(table(train$Sex, train$Survived), 1)
+  test$Survived <- 0
+  test$Survived[test$Sex == 'female'] <- 1
 }
