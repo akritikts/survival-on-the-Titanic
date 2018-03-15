712745f470260263ebf560b075ab3140fa9df0a3
akritikts <akritikts@gmail.com>
2018-03-15 11:53:26 +0530

    the all dead script

--- prediction_script.R
+++ prediction_script.R
@@ -3,4 +3,7 @@ manip <- function(){
   train$Survived
   table(train$Survived)
   prop.table(table(train$Survived))
+  test$Survived <- rep(0, 418)
+  sub_pred <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+  > write.csv(sub_pred, file = "theyallperish.csv", row.names = FALSE)
 }
