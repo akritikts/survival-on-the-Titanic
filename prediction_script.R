89dea004cd10c7ac0110ae15a9a21adef502137e
akritikts <akritikts@gmail.com>
2018-03-19 11:42:38 +0530

    decision trees implemented on family

--- prediction_script.R
+++ prediction_script.R
@@ -59,4 +59,9 @@ manip <- function(){
   combi$FamilyID <- factor(combi$FamilyID)
   train <- combi[1:891, ]
   test <- combi[892:1309, ]
+  fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+               +              data=train, 
+               +              method="class")
+  plot(fit)
+  rpart.plot(fit)
 }
